cd36a4544343b587d3e3f53a3f9f42c9d7ee14bb
VojtaB <brozek@thepay.cz>
2021-06-23 18:44:51 +0200

    fix typos in config comments and add strict types in config

--- config/performance_log.php
+++ config/performance_log.php
@@ -1,12 +1,14 @@
 <?php
 
+declare(strict_types=1);
+
 return [
-    // configure name of log chanel defined in config/logging.php used for logging
-    // with null value default log chanel will be used
+    // configure name of log channel defined in config/logging.php used for logging
+    // with null value default log channel will be used
     'log_channel' => null,
     // thresholds definitions, if anything run longer than threshold service will log warning
     // all thresholds are float value in milliseconds
-    // null threshold will turn off any measurring
+    // null threshold will turn off any measuring
     // threshold zero value with 'app.debug' true value will log running time for anything as debug
     'database' => [
         # define threshold for single database query
