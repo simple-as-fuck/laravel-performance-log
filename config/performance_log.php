68603c1201ad4d68622fd7af3de672476e5f6a04
VojtaB <brozek@thepay.cz>
2021-07-08 11:21:44 +0200

    change line comments in config to // style

--- config/performance_log.php
+++ config/performance_log.php
@@ -11,18 +11,18 @@ return [
     // null threshold will turn off any measuring
     // threshold zero value with 'app.debug' true value will log running time for anything as debug
     'database' => [
-        # define threshold for single database query
+        // define threshold for single database query
         'slow_query_threshold' => 50,
 
-        # define threshold for one database transaction
+        // define threshold for one database transaction
         'slow_transaction_threshold' => 300,
     ],
     'migration' => [
-        # define threshold for single database query executed in migrations
+        // define threshold for single database query executed in migrations
         'slow_query_threshold' => 150,
     ],
     'http' => [
-        # define threshold for whole http request
+        // define threshold for whole http request
         'slow_request_threshold' => 1000,
     ],
 ];
